0c52cf2d9fa696def19cdb0f1863c8ce53a95ba3
noa968 <t246773@st.yamagata-u.ac.jp>
2025-10-24 19:26:40 +0900

    Add files via upload

---
+++ hensu1.c
@@ -0,0 +1,32 @@
+#include<float.h>
+#include<limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("double型の最大値:%g\n",DBL_MAX);
+    printf("double型の最小値:%g\n",DBL_MIN);
+    printf("double型の精度:%d\n",DBL_DIG);
+    
+    printf("long double型の最大値:%Le\n",LDBL_MAX);
+    printf("long double型の最小値:%Le\n",LDBL_MIN);
+    printf("long double型の精度:%d\n",LDBL_DIG);
+    
+    printf("int型の最大値:%d\n",INT_MAX);
+    printf("int型の最小値:%d\n",INT_MIN);
+    printf("int型の符号なし最大値:%d\n",UINT_MAX);
+    
+    printf("char型の最大値:%d\n",CHAR_MAX);
+    printf("char型の最小値:%d\n",CHAR_MIN);
+    printf("char型の符号なし最大値:%d\n",UCHAR_MAX);
+    
+    printf("short int型の最大値:%d\n",SHRT_MAX);
+    printf("short int型の最小値:%d\n",SHRT_MIN);
+    printf("short型の符号なし最大値:%d\n",USHRT_MAX);
+    
+    printf("long long int型の最大値:%lli\n",LLONG_MAX);
+    printf("long long int型の最小値:%lli\n",LLONG_MIN);
+    
+
+    return 0;
+}
